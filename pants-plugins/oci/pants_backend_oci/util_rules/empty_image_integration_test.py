590e7c75732e57abf21708de9c5e277c71742b72
Tom Solberg <me@sbg.dev>
2023-05-25 15:20:00 +0200

    fix test

--- pants-plugins/oci/pants_backend_oci/util_rules/empty_image_integration_test.py
+++ pants-plugins/oci/pants_backend_oci/util_rules/empty_image_integration_test.py
@@ -51,10 +51,10 @@ def test_package_empty_with_file() -> None:
 
         result.assert_success()
 
-        output_path = os.path.dirname(os.path.abspath(tmpdir)) + f"/dist/{tmpdir}_oci_oci.None"
+        output_path = (os.path.dirname(os.path.abspath(tmpdir)) + f"/dist/{tmpdir}.oci/oci.tar.d",)
 
-        tar = tarfile.open(output_path)
-        json_data = json.load(tar.extractfile("index.json"))
+        with open(f"{output_path}/index.json", "r") as f:
+            json_data = json.load(f)
 
         # only one tag should exist - always, but let's just double-check.
         assert len(json_data["manifests"]) == 1
@@ -63,13 +63,15 @@ def test_package_empty_with_file() -> None:
         digest = json_data["manifests"][0]["digest"][7:]
 
         # The contents of the head digest describes all layers in the tag
-        json_data = json.load(tar.extractfile(f"blobs/sha256/{digest}"))
+        with open(f"{output_path}/blobs/sha256/{digest}", "r") as f:
+            json_data = json.load(f)
 
         # The digest of the last layer (most recently added)
         layer_digest = json_data["layers"][-1]["digest"][7:]
 
         # We assume that all layers are tar+gzip, so we need to read tar-from-tar
-        layer_data = tarfile.open(fileobj=tar.extractfile(f"blobs/sha256/{layer_digest}"))
+        with open(f"{output_path}/blobs/sha256/{layer_digest}", "r") as f:
+            layer_data = tarfile.open(fileobj=f)
 
         # We added a single file above, so should only see that here
         assert layer_data.getnames() == [f"{tmpdir}/oci/file.txt"]
