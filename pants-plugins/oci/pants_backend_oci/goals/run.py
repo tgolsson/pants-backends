d305334787f5f435ad115b1b8cf6fbc30242d67a
Tom Solberg <me@sbg.dev>
2023-06-08 21:51:34 +0200

    hmm

--- pants-plugins/oci/pants_backend_oci/goals/run.py
+++ pants-plugins/oci/pants_backend_oci/goals/run.py
@@ -37,7 +37,7 @@ from pants_backend_oci.util_rules.image_bundle import (
 )
 from pants_backend_oci.util_rules.unpack import UnpackedImageBundleRequest
 
-if PANTS_SEMVER > Version("2.16.0a0"):
+if PANTS_SEMVER > Version("2.16.0dev0"):
     from pants.core.goals.run import RunInSandboxBehavior
 
 
@@ -47,7 +47,7 @@ class RunImageBundleCommand(RunFieldSet):
 
     repository: ImageRepository
     run_tty: ImageRunTty
-    if PANTS_SEMVER >= Version("2.16.0a0"):
+    if PANTS_SEMVER >= Version("2.16.0dev0"):
         run_in_sandbox_behavior = RunInSandboxBehavior.RUN_REQUEST_HERMETIC
 
 
@@ -82,7 +82,7 @@ async def prepare_run_image_bundle(
         rationale="runc",
         search_path=SEARCH_PATHS,
     )
-    if PANTS_SEMVER < Version("2.16.0a0"):
+    if PANTS_SEMVER < Version("2.16.0dev0"):
         kwargs["output_directory"] = "bins"
 
     binary_shims = BinaryShimsRequest.for_binaries(
@@ -172,7 +172,7 @@ async def prepare_run_image_bundle(
         Digest, CreateDigest([FileContent("run.sh", "\n".join(components).encode("utf-8"))])
     )
 
-    if PANTS_SEMVER >= Version("2.16.0a0"):
+    if PANTS_SEMVER >= Version("2.16.0dev0"):
         immutable_input_digests = shims.immutable_input_digests
         env = {"PATH": shims.path_component, "XDG_RUNTIME_DIR": "{chroot}/tmp"}
         input_digest = await Get(Digest, MergeDigests((rundir, tool.digest, script_digest)))
@@ -231,7 +231,7 @@ async def run_oci_command_repl(request: OciRepl) -> ReplRequest:
     )
 
 
-if PANTS_SEMVER < Version("2.16.0a0"):
+if PANTS_SEMVER < Version("2.16.0dev0"):
 
     @rule
     async def run_debug_oci_command_target(
@@ -246,7 +246,7 @@ def rules():
         UnionRule(ReplImplementation, OciRepl),
     ]
 
-    if PANTS_SEMVER >= Version("2.16.0a0"):
+    if PANTS_SEMVER >= Version("2.16.0dev0"):
         rules.extend(RunImageBundleCommand.rules())
     else:
         rules.append(UnionRule(RunFieldSet, RunImageBundleCommand))
