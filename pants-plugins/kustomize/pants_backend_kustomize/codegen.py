ae29ec11700f51549b26d78366924522afb763b0
Tom Solberg <ts@MSI>
2024-02-14 22:54:55 +0100

    Add `git` to codegen Kustomize invocations

--- pants-plugins/kustomize/pants_backend_kustomize/codegen.py
+++ pants-plugins/kustomize/pants_backend_kustomize/codegen.py
@@ -1,6 +1,8 @@
 import os
+from dataclasses import dataclass
 
 from pants.core.util_rules.external_tool import DownloadedExternalTool, ExternalToolRequest
+from pants.core.util_rules.system_binaries import BinaryShims, BinaryShimsRequest
 from pants.engine.fs import AddPrefix, Digest, MergeDigests, Snapshot
 from pants.engine.platform import Platform
 from pants.engine.process import Process, ProcessResult
@@ -8,6 +10,7 @@ from pants.engine.rules import Get, MultiGet, collect_rules, rule
 from pants.engine.target import GeneratedSources, GenerateSourcesRequest
 from pants.engine.unions import UnionRule
 from pants.source.source_root import SourceRoot, SourceRootRequest
+from pants.version import PANTS_SEMVER, Version
 
 from pants_backend_k8s.target_types import KubernetesSourceField
 from pants_backend_kustomize.subsystem import KustomizeTool
@@ -16,6 +19,48 @@ from pants_backend_kustomize.util_rules.prepare_context import (
     KustomizationContext,
     KustomizationContextRequest,
 )
+
+
+@dataclass(frozen=True)
+class GitRequest:
+    pass
+
+
+if PANTS_SEMVER >= Version("2.19.0.dev0"):
+    from pants.core.util_rules.system_binaries import SystemBinariesSubsystem
+
+    @rule
+    async def get_binary_shims(
+        _: GitRequest, system_binaries_subsystem: SystemBinariesSubsystem.EnvironmentAware
+    ) -> BinaryShims:
+        kwargs = dict(
+            rationale="asdqwe",
+            search_path=system_binaries_subsystem.system_binary_paths,
+        )
+
+        binary_shims = BinaryShimsRequest.for_binaries(
+            "git",
+            **kwargs,
+        )
+
+        return await Get(BinaryShims, BinaryShimsRequest, binary_shims)
+
+else:
+    from pants.core.util_rules.system_binaries import SEARCH_PATHS
+
+    @rule
+    async def get_binary_shims(_: GitRequest) -> BinaryShims:
+        kwargs = dict(
+            rationale="asdqwe",
+            search_path=SEARCH_PATHS,
+        )
+
+        binary_shims = BinaryShimsRequest.for_binaries(
+            "git",
+            **kwargs,
+        )
+
+        return await Get(BinaryShims, BinaryShimsRequest, binary_shims)
 
 
 class GenerateKubernetesFromKustomizeRequest(GenerateSourcesRequest):
@@ -29,13 +74,14 @@ async def generate_kubernetes_from_kustomize(
     kustomize: KustomizeTool,
     platform: Platform,
 ) -> GeneratedSources:
-    (context, kustomize) = await MultiGet(
+    context, kustomize, git = await MultiGet(
         Get(KustomizationContext, KustomizationContextRequest(request.protocol_target)),
         Get(
             DownloadedExternalTool,
             ExternalToolRequest,
             kustomize.get_request(platform),
         ),
+        Get(BinaryShims, GitRequest()),
     )
 
     merged_digest = await Get(
@@ -60,8 +106,10 @@ async def generate_kubernetes_from_kustomize(
                 output_files,
                 root_dir,
             ),
+            env={"PATH": git.path_component},
             input_digest=merged_digest,
-            description=f"Generating Python sources from {request.protocol_target.address}.",
+            immutable_input_digests=git.immutable_input_digests,
+            description=f"Generating Kustomize sources from {request.protocol_target.address}.",
             output_files=(output_files,),
         ),
     )
